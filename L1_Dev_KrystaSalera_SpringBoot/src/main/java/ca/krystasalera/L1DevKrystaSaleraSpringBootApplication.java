48c3ae3bb083932014342d6566a99a3af6bccb55
salerak <salerak@sheridancollege.ca>
2016-03-26 03:29:59 -0400

    Added https

--- L1_Dev_KrystaSalera_SpringBoot/src/main/java/ca/krystasalera/L1DevKrystaSaleraSpringBootApplication.java
+++ L1_Dev_KrystaSalera_SpringBoot/src/main/java/ca/krystasalera/L1DevKrystaSaleraSpringBootApplication.java
@@ -1,9 +1,16 @@
 package ca.krystasalera;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class L1DevKrystaSaleraSpringBootApplication {
@@ -15,4 +22,35 @@ public class L1DevKrystaSaleraSpringBootApplication {
 		logger.info("entered application");
 		SpringApplication.run(L1DevKrystaSaleraSpringBootApplication.class, args);
 	}
+	
+	
+	 @Bean
+	  public EmbeddedServletContainerFactory servletContainer() {
+	    TomcatEmbeddedServletContainerFactory tomcat =
+	      new TomcatEmbeddedServletContainerFactory() {
+	      
+	        @Override
+	        protected void postProcessContext(Context context) {
+	          SecurityConstraint securityConstraint = new SecurityConstraint();
+	          securityConstraint.setUserConstraint("CONFIDENTIAL");
+	          SecurityCollection collection = new SecurityCollection();
+	          collection.addPattern("/*");
+	          securityConstraint.addCollection(collection);
+	          context.addConstraint(securityConstraint);
+	        }
+	      };
+	    tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+	    return tomcat;
+	  }
+	  
+	  private Connector createHttpConnector() {
+	    Connector connector =
+	      new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	    connector.setScheme("http");
+	    connector.setSecure(false);
+	    connector.setPort(8080);
+	    connector.setRedirectPort(8443);
+	    return connector;
+	  }
+	
 }
--- L1_Dev_KrystaSalera_SpringBoot/src/main/java/ca/krystasalera/configuration/SecurityConfiguration.java
+++ L1_Dev_KrystaSalera_SpringBoot/src/main/java/ca/krystasalera/configuration/SecurityConfiguration.java
@@ -10,7 +10,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        //httpSecurity.authorizeRequests().antMatchers("/").permitAll();
     }
+    
+
+
 
 }
